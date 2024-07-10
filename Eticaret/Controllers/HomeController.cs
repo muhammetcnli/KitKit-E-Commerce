e3478761956a0585be25c94f676d784a1c9ddc50
Muhammet Canlı <howardroark756@gmail.com>
2024-07-10 09:20:48 +0300

    m

--- Eticaret/Controllers/HomeController.cs
+++ Eticaret/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
         var products = Repository.Products;
 
         if(!String.IsNullOrEmpty(searchString)){
-            products = products.Where(p=>p.Name.ToLower().Contains(searchString)).ToList();
+            products = products.Where(p=>p.Name!.ToLower().Contains(searchString)).ToList();
         }
          if(!String.IsNullOrEmpty(category) && category != "0"){
             products = products.Where(p=>p.CategoryId == int.Parse(category)).ToList();
@@ -39,15 +39,37 @@ public class HomeController : Controller
 
     [HttpGet]
     public IActionResult Create(){
-
         
         ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
         return View();
     }
 
     [HttpPost]
-    public IActionResult Create(Product model){
+    public async Task<IActionResult> Create(Product model, IFormFile imageFile){
+
+        var allowedExtensions = new[] {".jpg", ".jpeg", ".png", ".webp"};
+        var extension = Path.GetExtension(imageFile.FileName);
+        var randomFilName = string.Format($"{Guid.NewGuid().ToString()}{extension}");
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img", randomFilName);
+
+        if(imageFile != null){
+            if(!allowedExtensions.Contains(extension)){
+                ModelState.AddModelError("", "Geçerli bir resim seçiniz.");
+            }
+        }
+
+        if(ModelState.IsValid){
+            if(imageFile != null){
+            using(var stream = new FileStream(path, FileMode.Create)){
+                await imageFile.CopyToAsync(stream);
+            }
+            }
+        model.Image = randomFilName;
+        model.ProductId = Repository.Products.Count;
         Repository.CreateProduct(model);
         return RedirectToAction("Admin");
+        }
+        ViewBag.Categories = new SelectList(Repository.Categories, "CategoryId", "Name");
+        return View(model);
     }
 }   
--- Eticaret/Models/Product.cs
+++ Eticaret/Models/Product.cs
@@ -7,18 +7,24 @@ namespace Eticaret.Models
         [Display(Name ="Ürün Id")]
         public int ProductId {get;set;}
 
-        [Display(Name ="Ürün Adı")]
-        [Required]
-        public string Name {get;set;} = string.Empty;
+        [Display(Name ="Adı")]
+        [Required(ErrorMessage ="Ürün adı giriniz")]
+        [StringLength(100)]
+        public string? Name {get;set;}
 
-        [Display(Name ="Ürün Fiyatı")]
-        public decimal Price {get;set;}
+        [Display(Name ="Fiyatı")]
+        [Required(ErrorMessage ="Ürün fiyatı giriniz")]
+        [Range(0, 105000, ErrorMessage ="0 ile 105000 arası fiyat girin")]
+        public decimal? Price {get;set;}
 
-        [Display(Name ="Ürün Fotografı")]
-        public string Image {get;set;} = string.Empty;
+        [Display(Name ="Fotografı")]
+
+        public string? Image {get;set;} = string.Empty;
 
         [Display(Name ="Aktif mi?")]
         public bool IsActive {get;set;}
+        [Display(Name ="Kategorisi")]
+        [Required]
         public int CategoryId {get;set;}
     }
 }
